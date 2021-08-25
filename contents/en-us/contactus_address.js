380ca591855a695c6ae437579970e1c750375894
Hibri Marzook <hibri@hibri.net>
2021-08-25 16:24:48 +0100

    Update contactus_address.js

--- contents/en-us/contactus_address.js
+++ contents/en-us/contactus_address.js
@@ -1,1 +1,1 @@
-var addressFormatHTML='<ul><li class="GC4"><span>Email (orders)</span>:&nbsp;<a href="mailto:sales@primapearls.com">sales@primapearls.com</a></li><li class="GC4">http://www.primapearls.com</li></ul>';
+var addressFormatHTML='<ul><li class="GC4 AddressformatTitle"><span>Email (orders)</span>:&nbsp;<a href="mailto:sales@primapearls.com">sales@primapearls.com</a></li><li class="GC4">http://www.primapearls.com</li></ul>';
